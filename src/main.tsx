8ea5c6a8d69754d946dbb1c790fa2b4d6a6c238d
Gboyega <gboyega.banjo@gmail.com>
2026-01-07 21:30:11 +0100

    initial setup

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { createRouter, RouterProvider } from "@tanstack/react-router"
+import { routeTree } from "./routeTree.gen"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import "./index.css"
+
+const queryClient = new QueryClient()
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
+})
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
+)
